ac2af2c68d56d51e7a41f1ddf41257cad31ef26b
fjordimm <fjordimm@gmail.com>
2026-01-11 15:37:16 -0800

    asgn0: finished part 3

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -1,31 +1,42 @@
+
+var canvas = null;
+var ctx = null;
+
 // DrawTriangle.js (c) 2012 matsuda
 function main() {
     // Retrieve <canvas> element
-    var canvas = document.getElementById('example');
+    canvas = document.getElementById('example');
     if (!canvas) {
         console.log('Failed to retrieve the <canvas> element');
         return false;
     }
 
     // Get the rendering context for 2DCG
-    var ctx = canvas.getContext('2d');
-
-    // // Draw a blue rectangle
-    // ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-    // ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+    ctx = canvas.getContext('2d');
 
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 400, 400);
 
-    function drawVector(v, color) {
-        ctx.strokeStyle = color;
+    let v1 = new Vector3([1, 1, 0]);
+    drawVector(v1, "red");
+}
 
-        ctx.beginPath();
-        ctx.moveTo(200, 200);
-        ctx.lineTo(200 + v.elements[0] * 20, 200 - v.elements[1] * 20);
-        ctx.stroke();
-    }
+function drawVector(v, color) {
+    ctx.strokeStyle = color;
 
-    let v1 = new Vector3([2.25, 2.25, 0]);
+    ctx.beginPath();
+    ctx.moveTo(200, 200);
+    ctx.lineTo(200 + 20 * v.elements[0], 200 - 20 * v.elements[1]);
+    ctx.stroke();
+}
+
+function handleDrawEvent() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 400, 400);
+
+    xInput = document.getElementById("x-input").value;
+    yInput = document.getElementById("y-input").value;
+
+    let v1 = new Vector3([xInput, yInput, 0]);
     drawVector(v1, "red");
 }
