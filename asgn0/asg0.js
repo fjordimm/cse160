8e123ac3d7b0020d1df8c9f1ab6377d4613d380b
fjordimm <fjordimm@gmail.com>
2026-01-11 15:42:04 -0800

    asgn0: did part 4

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -17,8 +17,7 @@ function main() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 400, 400);
 
-    let v1 = new Vector3([1, 1, 0]);
-    drawVector(v1, "red");
+    handleDrawEvent();
 }
 
 function drawVector(v, color) {
@@ -34,9 +33,13 @@ function handleDrawEvent() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 400, 400);
 
-    xInput = document.getElementById("x-input").value;
-    yInput = document.getElementById("y-input").value;
+    v1x = document.getElementById("v1-x-input").value;
+    v1y = document.getElementById("v1-y-input").value;
+    v2x = document.getElementById("v2-x-input").value;
+    v2y = document.getElementById("v2-y-input").value;
 
-    let v1 = new Vector3([xInput, yInput, 0]);
+    let v1 = new Vector3([v1x, v1y, 0]);
+    let v2 = new Vector3([v2x, v2y, 0]);
     drawVector(v1, "red");
+    drawVector(v2, "blue");
 }
