dacbfedb16e1e4a712181d978777ab139772d484
fjordimm <fjordimm@gmail.com>
2026-01-11 15:07:38 -0800

    asgn0: did part 2

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -1,16 +1,31 @@
 // DrawTriangle.js (c) 2012 matsuda
-function main() {  
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('example');  
-  if (!canvas) { 
-    console.log('Failed to retrieve the <canvas> element');
-    return false; 
-  } 
+function main() {
+    // Retrieve <canvas> element
+    var canvas = document.getElementById('example');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return false;
+    }
 
-  // Get the rendering context for 2DCG
-  var ctx = canvas.getContext('2d');
+    // Get the rendering context for 2DCG
+    var ctx = canvas.getContext('2d');
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+    // // Draw a blue rectangle
+    // ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
+    // ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 400, 400);
+
+    function drawVector(v, color) {
+        ctx.strokeStyle = color;
+
+        ctx.beginPath();
+        ctx.moveTo(200, 200);
+        ctx.lineTo(200 + v.elements[0] * 20, 200 - v.elements[1] * 20);
+        ctx.stroke();
+    }
+
+    let v1 = new Vector3([2.25, 2.25, 0]);
+    drawVector(v1, "red");
 }
