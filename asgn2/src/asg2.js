5b57892123086ad8bef84727aead3409b6026775
fjordimm <fjordimm@gmail.com>
2026-01-29 10:29:10 -0800

    setup for asgn2

---
+++ asgn2/src/asg2.js
@@ -0,0 +1,86 @@
+var VSHADER_SOURCE = `
+attribute vec4 a_Position;
+uniform float u_Size;
+void main() {
+    gl_Position = a_Position;
+    gl_PointSize = u_Size;
+}
+`;
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+precision mediump float;
+uniform vec4 u_FragColor;
+void main() {
+    gl_FragColor = u_FragColor;
+}
+`;
+
+// WebGL Globals
+let canvas;
+let gl;
+let vertexBuffer;
+let a_Position;
+let u_FragColor;
+let u_Size;
+
+function setupWebGL() {
+    canvas = document.getElementById('webgl');
+
+    gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create buffer object');
+        return;
+    }
+}
+
+function setupGLSLVariables() {
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    // Get the storage location of a_Position
+    a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    // Get the storage location of u_FragColor
+    u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (!u_FragColor) {
+        console.log('Failed to get the storage location of u_FragColor');
+        return;
+    }
+
+    u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+    if (!u_Size) {
+        console.log('Failed to get the storage location of u_Size');
+        return;
+    }
+}
+
+// Other Globals
+let listOfShapes = [];
+let shapeType = "point";
+
+function main() {
+    setupWebGL();
+    setupGLSLVariables();
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    listOfShapes = [];
+
+    canvas.onmousedown = function (ev) { handleMouseClick(ev) };
+    canvas.onmousemove = function (ev) { handleMouseMove(ev) };
+}
