80f4f7fce4b87b87429f74147eb505ba83cd79c0
fjordimm <fjordimm@gmail.com>
2026-02-11 20:23:21 -0800

    asgn3: minor change

--- asgn3/shaders/fragment_shader.glsl
+++ asgn3/shaders/fragment_shader.glsl
@@ -9,5 +9,4 @@ varying vec2 v_UV;
 void main() {
     vec4 textureColor = texture2D(u_Texture, v_UV);
     gl_FragColor = (1.0 - u_TextureWeight) * u_FragColor + u_TextureWeight * textureColor;
-    // gl_FragColor = 0.5 * u_FragColor + 0.5 * textureColor;
 }
